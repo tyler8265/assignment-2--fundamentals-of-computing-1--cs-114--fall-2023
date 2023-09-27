c229a6a0b48cc7f271ba6d3fb8b04d15543a95bf
Tyler Bertrand <132798327+tyler8265@users.noreply.github.com>
2023-09-27 18:08:39 -0400

    Update Diamond.java

--- Diamond.java
+++ Diamond.java
@@ -1,4 +1,11 @@
+import java.util.Scanner;
+
 public class Diamond {
   public static void main(String[] args) {
+    int i;
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter a number");
+    i = scan.nextInt();
+    scan.close();
   }
 }
