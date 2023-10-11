e0974998195aa9b9dee0efc4dbe6f7ca5877ebc2
Tyler Bertrand <132798327+tyler8265@users.noreply.github.com>
2023-10-11 19:21:31 -0400

    Update Diamond.java

--- Diamond.java
+++ Diamond.java
@@ -2,22 +2,21 @@ import java.util.Scanner;
 
 public class Diamond {
   public static void main(String[] args) {
-    double input;
-    int i, j, as;
+    double input, spaces, downToEnd, upToMiddle;
+    int i, j, as, numberOfAsteriks;
     int row = 0;
     Scanner scan = new Scanner(System.in);
-    System.out.println("Enter a number");
+    System.out.println("Enter a number "); //Requests any number from the user
     input = scan.nextInt();
-    double spaces = (input / 2);
-    System.out.println(spaces);
-    double uptomiddle = Math.ceil(spaces);
-    int numberofasteriks = 1;
-    double downtoend = input - uptomiddle;
+    spaces = (input / 2); //Finds Number of Spaces
+    upToMiddle = Math.ceil(spaces);
+    numberOfAsteriks = 1;
+    downToEnd = input - upToMiddle;
 
     //Code for Odd
     //Top Half Odd
     if (input % 2 == 1){
-      for (i = 0; i < uptomiddle; i++){
+      for (i = 0; i < upToMiddle; i++){
         for (j = 0; j < spaces - 1; j++){
           System.out.print(" ");
         }
@@ -27,19 +26,19 @@ public class Diamond {
           System.out.print("*");
         }
         else{
-          for(as = 0; as < numberofasteriks; as++)
+          for(as = 0; as < numberOfAsteriks; as++)
             System.out.print("*");
         }
         for (j = 0; j < spaces; j++){
           System.out.print(" ");
         }
-        numberofasteriks += 2;
+        numberOfAsteriks += 2;
         System.out.println();
         //Bottom Half Odd
       }
-      numberofasteriks -= 2;
-      for (i = 0; i < downtoend; i++){
-        numberofasteriks -= 2;
+      numberOfAsteriks -= 2;
+      for (i = 0; i < downToEnd; i++){
+        numberOfAsteriks -= 2;
         for (j = 0; j < spaces + 1; j++)
           System.out.print(" ");
         spaces++;
@@ -48,7 +47,7 @@ public class Diamond {
           System.out.print("*");
         }
         else{
-          for(as = 0; as < numberofasteriks; as++)
+          for(as = 0; as < numberOfAsteriks; as++)
             System.out.print("*");
         }
         for (j = 0; j < spaces; j++){
@@ -60,7 +59,7 @@ public class Diamond {
     //Code for Even
     //Code for Top Half Even
     else{
-      for (i = 0; i < uptomiddle; i++){
+      for (i = 0; i < upToMiddle; i++){
         for (j = 0; j < spaces - 1; j++)
           System.out.print(" ");
         spaces--;
@@ -69,23 +68,23 @@ public class Diamond {
           System.out.print(" *");
         }
         else{
-          for (as = 0; as < numberofasteriks; as++)
+          for (as = 0; as < numberOfAsteriks; as++)
             System.out.print(" *");
         }
         for (j = 0; j < spaces - 1; j++)
           System.out.print(" ");
-        numberofasteriks += 1;
+        numberOfAsteriks += 1;
         System.out.println();
       }
       //Code for Bottom Half Even
-      numberofasteriks -= 1;
-      for (i = 0; i < downtoend; i++){
-        numberofasteriks -= 1;
+      numberOfAsteriks -= 1;
+      for (i = 0; i < downToEnd; i++){
+        numberOfAsteriks -= 1;
         for (j = 0; j < spaces + 1; j++)
           System.out.print(" ");
         spaces++;
         row++;
-        for(as = 0; as < numberofasteriks; as++)
+        for(as = 0; as < numberOfAsteriks; as++)
           System.out.print(" *");
         for (j = 0; j < spaces; j++){
           System.out.print(" ");
